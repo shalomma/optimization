0fa2719fa87af01ce50ab933e7800b2bdee3ff31
Maayan Shalom <sha.maayan@gmail.com>
2020-05-25 01:56:26 +0300

    create funcs

--- newton.py
+++ newton.py
@@ -1,9 +1,21 @@
+import numpy as np
+
+
 class NewtonOptimizer:
     def __init__(self, func, alpha, init):
-        pass
+        self.func = func
+        self.alpha = alpha
+        self.x = init
 
-    def step(self):
-        pass
+    def step(self, x):
+        gx = self.func.grad(x)
+        hx = self.func.hessian(x)
+        next_x = x - self.alpha * np.linalg.inv(hx).dot(gx)
+        return next_x, gx, hx
 
     def optimize(self, threshold, max_iters):
-        pass
+        for i in range(max_iters):
+            next_x, gx, hx = self.step(self.x)
+            if np.linalg.norm(next_x - self.x, ord=1) < threshold:
+                break
+            self.x = next_x
