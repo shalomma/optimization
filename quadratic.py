886cb326cc71b10b726b854f05cbfc50ecc38595
Maayan Shalom <sha.maayan@gmail.com>
2020-05-26 00:31:34 +0300

    fixed grad

--- quadratic.py
+++ quadratic.py
@@ -7,7 +7,7 @@ class QuadraticFunction:
         return 0.5 * x.T.dot(self.Q).dot(x) + self.b.T.dot(x)
 
     def grad(self, x):
-        return self.Q.dot(x)
+        return (self.Q.T + self.Q).dot(x) + self.b
 
     def hessian(self, x):
-        return self.Q
+        return self.Q.T + self.Q
