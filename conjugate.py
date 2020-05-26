daac6c1746f9eecbc6542977205453ac0bdf9594
Maayan Shalom <sha.maayan@gmail.com>
2020-05-26 22:04:30 +0300

    return updates

--- conjugate.py
+++ conjugate.py
@@ -2,33 +2,35 @@ class ConjGradOptimizer:
     def __init__(self, func, init):
         self.func = func
         self.x = init
-        self.d = - self.func.grad(init)
+        self.g = self.func.grad(init)
+        self.g_prev = None
+        self.d = - self.g
+        self.dim = len(init)
         self.alpha = None
-        self.x_prev = None
+        self.x_prev = init
 
     def update_grad(self):
         return self.func.grad(self.x), self.func.grad(self.x_prev)
 
     def update_dir(self):
-        g, g_prev = self.update_grad()
-        beta = g.dot(g) / g_prev.dot(g_prev)
-        self.d = - self.func.grad(self.x) + beta * self.d
+        beta = self.g.dot(self.g) / self.g_prev.dot(self.g_prev)
+        return - self.g + beta * self.d
 
     def update_alpha(self):
-        g, _ = self.update_grad()
-        self.alpha = g.T.dot(self.d) / self.d.T.dot(self.func.Q).dot(self.d)
+        return - self.g.T.dot(self.d) / self.d.T.dot(self.func.Q).dot(self.d)
 
     def update_x(self):
         self.x_prev = self.x
-        self.x += self.alpha * self.d
+        return self.x + self.alpha * self.d
 
     def step(self):
-        self.update_alpha()
-        self.update_x()
-        self.update_dir()
+        self.alpha = self.update_alpha()
+        self.x = self.update_x()
+        self.g, self.g_prev = self.update_grad()
+        self.d = self.update_dir()
 
     def optimize(self):
         i = 0
-        for i in range(10):  # TODO: understand the stopping criteria
+        for i in range(self.dim):
             self.step()
         return self.func(self.x), self.x, i
