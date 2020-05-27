4e5f3163610b18b818731291f79d8bf0eafeb8ea
Maayan Shalom <sha.maayan@gmail.com>
2020-05-28 02:02:06 +0300

    Fast hessian

--- fast_route.py
+++ fast_route.py
@@ -1,4 +1,5 @@
 import numpy as np
+from newton import NewtonOptimizer
 
 
 class FastRoute:
@@ -7,6 +8,7 @@ class FastRoute:
         self.finish_x, self.finish_y = finish_x, finish_y
         self.velocities = velocities
         self.d = (self.finish_y - self.start_y) / len(velocities)
+        self.dim = len(velocities) - 1
 
     def __call__(self, x):
         x = np.concatenate((x, [self.finish_x]))
@@ -22,25 +24,44 @@ class FastRoute:
     def grad(self, x):
         x = np.concatenate((x, [self.finish_x]))
         x_prev = self.start_x
-        r = []
+        r = np.zeros(self.dim + 1)
         for i, x_i in enumerate(x):
             x_delta = x_i - x_prev
             hypotenuse = np.sqrt(x_delta ** 2 + self.d ** 2)
-            r.append(x_delta / (self.velocities[i] * hypotenuse))
+            r[i] = x_delta / (self.velocities[i] * hypotenuse)
             x_prev = x_i
-        g = []
-        for i in range(len(x) - 1):
-            g.append(r[i] + r[i + 1])
-        return np.array(g)
+        return r[:self.dim] - r[1:self.dim + 1]
 
     def hessian(self, x):
-        pass
+        x = np.concatenate((x, [self.finish_x]))
+        x_prev = self.start_x
+        p = np.zeros(self.dim + 1)
+        for i, x_i in enumerate(x):
+            x_delta = x_i - x_prev
+            hypotenuse = np.sqrt(x_delta ** 2 + self.d ** 2)
+            p[i] = self.d ** 2 / (self.velocities[i] * (hypotenuse ** 3))
+        h = np.zeros((self.dim, self.dim))
+        h[range(1, self.dim), range(0, self.dim - 1)] = - p[1:self.dim]
+        h = h.T + h
+        h[range(self.dim), range(self.dim)] = p[:self.dim] + p[1:self.dim + 1]
+        return h
+
+
+def find_fast_route(objective, init, alpha=1., threshold=1e-3, max_iters=1e3):
+    opt = NewtonOptimizer(objective, alpha=alpha, init=init)
+    opt.optimize(threshold, max_iters)
+    return func(opt.x), opt.x, opt.num_iters
 
 
 if __name__ == '__main__':
-    velocities_ = np.array([1, 1, 1, 1, 1, 1, 1])
-    func = FastRoute(0, 0, 7, 7, velocities_)
-    x0 = np.array([1, 2, 3, 4, 5, 6])
+    velocities_ = np.array([4, 1, 1, 1])
+    func = FastRoute(start_x=0, start_y=0,
+                     finish_x=10, finish_y=10,
+                     velocities=velocities_)
 
-    fx = func(x0)
-    dfx = func.grad(x0)
+    x0 = np.array([1, 4, 6])
+
+    fx, x, i = find_fast_route(func, x0, alpha=0.01, threshold=1e-14)
+
+    dfx = func.grad(x)
+    hx = func.hessian(x)
